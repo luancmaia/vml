d2bc7846a8b790e744b9e5a3fd47f3df2e4919fa
luancuba <luancuba9@gmail.com>
2018-03-04 22:44:25 -0300

    removed logger

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import reducer from '../reducers';
 import promise from 'redux-promise-middleware';
 
-const middleware = applyMiddleware(promise(), createLogger());
+const middleware = applyMiddleware(promise());
 const store = createStore(reducer, middleware);
 
 export default store;
